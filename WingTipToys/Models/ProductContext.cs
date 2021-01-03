87f656d378ac6f590126902b1c4c7bdc05ac0543
coltontvb <coltontvb@gmail.com>
2021-01-02 19:09:49 -0600

    Add Product related entity models and update Global.* files to incorporate and init them

---
+++ WingTipToys/Models/ProductContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingTipToys.Models
+{
+    //Stores collections (tables) of tuples. Each tuple is a specific objects
+    public class ProductContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
